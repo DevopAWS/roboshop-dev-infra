611a5d93fcc7f6ff98d8aa6e0b84df32d23aeecc
dev1 <dev1@email.com>
2024-01-26 17:35:59 +0530

    dev

---
+++ 11-user/variables.tf
@@ -0,0 +1,32 @@
+variable "common_tags" {
+  default = {
+    Project     = "roboshop"
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
+
+variable "tags" {
+  default = {
+    Component = "user"
+  }
+
+}
+
+variable "project_name" {
+  default = "roboshop"
+}
+variable "environment" {
+  default = "dev"
+
+}
+
+variable "zone_name" {
+  default = "daws76.online"
+
+}
+
+variable "iam_instance_profile" {
+    default = "ansible-role"
+  
+}
